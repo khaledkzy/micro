fb4b7157278f752f92a46d4cb1c48b97bd465eb0
khaledkzy <khaled.kzy@gmail.com>
2023-06-07 18:17:56 +0100

    Implement sleuth and zipkin

--- alert/src/main/java/com/micro/AlertApplication.java
+++ alert/src/main/java/com/micro/AlertApplication.java
@@ -2,8 +2,10 @@ package com.micro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AlertApplication {
       public static void main(String[] args) {
         SpringApplication.run(AlertApplication.class, args);
