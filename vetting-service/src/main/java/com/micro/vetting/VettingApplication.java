ce7db407771122c467f148eb6d2a031179ddbcf7
khaledkzy <khaled.kzy@gmail.com>
2023-06-07 14:32:50 +0100

    Service Discovery Config for the Vetting-Service

--- vetting-service/src/main/java/com/micro/vetting/VettingApplication.java
+++ vetting-service/src/main/java/com/micro/vetting/VettingApplication.java
@@ -2,8 +2,10 @@ package com.micro.vetting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class VettingApplication {
     public static void main(String[] args) {
 
